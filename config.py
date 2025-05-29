1cbec0713c94395b9bfafe857255993fa6ab45f2
Santianchez <106118976+Santianchez@users.noreply.github.com>
2025-05-29 18:22:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-INFLUX_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
-INFLUX_TOKEN = "rnRx-Nk8dXeumEsQeDT4hk78QFWNTOVim7UrH5fnYKVSoQQIkhCwKq03-UMKN-S0Nj-DbfmrMD0HUI61qRJaiw=="
-ORG = "0925ccf91ab36478"
-BUCKET = "homeiot"
+INFLUX_URL = "https://us-east-1-1.aws.cloud2.influxdata.com" # [cite: 37]
+INFLUX_TOKEN = "rnRx-Nk8dXeumEsQeDT4hk78QFWNTOVim7UrH5fnYKVSoQQIkhCwKq03-UMKN-S0Nj-DbfmrMD0HUI61qRJaiw==" # [cite: 37]
+ORG = "0925ccf91ab36478" # [cite: 37]
+BUCKET = "homeiot" # [cite: 39]
